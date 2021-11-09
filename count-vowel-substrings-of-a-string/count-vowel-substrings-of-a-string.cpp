34461ee38cb4d57142a9ff3def9cb43fb20a4686
SHIKHAR BANSAL <shikharbansal007@gmail.com>
2021-11-09 11:24:23 +0530

    Time: 4 ms (86.25%), Space: 6.2 MB (84.56%) - LeetHub

--- count-vowel-substrings-of-a-string/count-vowel-substrings-of-a-string.cpp
+++ count-vowel-substrings-of-a-string/count-vowel-substrings-of-a-string.cpp
@@ -1,18 +1,46 @@
 class Solution {
+    
     bool isVowel(char c) {
         return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
-    };
+    }
+    
+    int countAtmostKVowel(string s, int k)
+    {
+        int i=0;
+        int n = s.size();
+        unordered_map<char, int> mp;    
+        int cnt =0;
+        
+        for(int j=0; j<n; j++)
+        {
+            if(!isVowel(s[j]))
+            {
+                mp.clear();
+                i = j+1;
+                continue;
+            }
+            mp[s[j]]++;
+            
+            while(mp.size() > k)
+            {
+                mp[s[i]]--;
+                if(mp[s[i]] == 0)
+                {
+                    mp.erase(s[i]);
+                }
+                i++;
+            }
+            
+            cnt += (j-i + 1);
+            
+        }
+        return cnt;
+    }
+    
 public:
     int countVowelSubstrings(string s) {
-        int ans = 0, N = s.size();
-        unordered_map<char, int> cnt;
-        for (int i = 0; i < N; ++i) {
-            cnt.clear();
-            for (int j = i; j < N && isVowel(s[j]); ++j) {
-                cnt[s[j]]++;
-                if (cnt.size() == 5) ++ans;
-            }
-        }
-        return ans;
+        
+        return countAtmostKVowel(s, 5) - countAtmostKVowel(s, 4);
+       
     }
 };
