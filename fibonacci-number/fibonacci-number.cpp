7bab646d802ce94832dcb03dc37dd5ef1961bc59
SHIKHAR BANSAL <shikharbansal007@gmail.com>
2021-11-29 17:43:07 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (10.86%) - LeetHub

--- fibonacci-number/fibonacci-number.cpp
+++ fibonacci-number/fibonacci-number.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
+    unordered_map<int, int> memo;
+    
     int fib(int n) {
         
-        if(n == 0 )
-            return 0;
+        if(n ==0 || n == 1)
+            return n;
         
-        if(n== 1)
-            return 1;
+        if(memo.find(n) != memo.end())
+            return memo[n];
         
-        return fib(n-1) + fib(n-2);
+        memo[n] = fib(n-1) + fib(n-2);
+        
+        return memo[n];
+        
+        
     }
 };
