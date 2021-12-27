f0edb6a054c3373ad5556fe44809b1ecf8effb02
SHIKHAR BANSAL <shikharbansal007@gmail.com>
2021-12-27 17:53:54 +0530

    Time: 8 ms (86.38%), Space: 20.9 MB (56.98%) - LeetHub

---
+++ boundary-of-binary-tree/boundary-of-binary-tree.cpp
@@ -0,0 +1,91 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+
+    bool isLeaf(TreeNode *root)
+    {
+        return !root->left && !root->right;
+    }
+    
+    void addLeftBoundary(TreeNode *root, vector<int>& res)
+    {
+        TreeNode *curr = root -> left;
+        
+        while(curr)
+        {
+            if(!isLeaf(curr))
+                res.push_back(curr -> val);
+            
+            if(curr -> left)
+                curr = curr -> left;
+            else
+                curr = curr -> right;
+        }
+            
+    }
+    
+    void addRightBoundary(TreeNode *root, vector<int>& res)
+    {
+        TreeNode *curr = root -> right;
+        vector<int> temp;
+        while(curr)
+        {
+            if(!isLeaf(curr))
+                temp.push_back(curr -> val);
+            
+            if(curr -> right)
+                curr = curr -> right;
+            else
+                curr = curr -> left;
+        }
+        
+        for(int i = temp.size() - 1; i>=0; i--)
+        {
+            res.push_back(temp[i]);
+        }
+    }
+    
+    
+    void addLeaves(TreeNode* root, vector<int>& res)
+    {
+        if(isLeaf(root))
+        {
+            res.push_back(root -> val);
+            return;
+        }
+        
+        if(root -> left)
+            addLeaves(root -> left, res);
+        if(root  -> right)
+            addLeaves(root -> right, res);
+    }
+    
+    
+    public:
+    vector<int> boundaryOfBinaryTree(TreeNode* root) {
+        vector<int> res;
+        
+        if(!root)
+            return res;
+        
+        if(!isLeaf(root))
+            res.push_back(root -> val);
+        
+        addLeftBoundary(root, res);
+        
+        addLeaves(root, res);
+        
+        addRightBoundary(root, res);
+        
+        return res;
+    }
+};
