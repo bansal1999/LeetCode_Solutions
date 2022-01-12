11ce8510b8da86d6e34d45a76a90350c846cfbe3
SHIKHAR BANSAL <shikharbansal007@gmail.com>
2022-01-12 22:51:19 +0530

    Added solution - LeetHub

--- Count Occurences of Anagrams - GFG/count-occurences-of-anagrams.cpp
+++ Count Occurences of Anagrams - GFG/count-occurences-of-anagrams.cpp
@@ -10,37 +10,55 @@ class Solution{
 public:
 	int search(string pat, string txt) {
 	    // code here
-	     unordered_map<char,int> umap;
-    int i=0,j=0,k=pat.length(),ans=0;
-    for(int l=0;l<pat.length();l++){
-        umap[pat[l]]++;
-    }
-    int count=umap.size();
-    while(j<txt.length()){
-        if(umap.find(txt[j])!=umap.end()){
-            umap[txt[j]]-=1;
-            if(umap[txt[j]]==0){
-                count-=1;
-            }
-        }
-        if(j-i+1<k){
-            j++;
-        }
-        else if(j-i+1==k){
-            if(count==0){
-                ans+=1;
-            }
-            if(umap.find(txt[i])!=umap.end()){
-                umap[txt[i]]+=1;
-                if(umap[txt[i]]==1){
-                    count+=1;
-                }
-            }
-            i++;
-            j++;
-        }
-    }
-    return ans;
+	    unordered_map<char, int> mp;
+	    
+	    for(int l = 0; l < pat.length(); l++)
+	    {
+	        mp[pat[l]]++;
+	    }
+	    
+	    int i =0, j =0, count = mp.size(), ans = 0, k = pat.length();
+	    
+	    while(j < txt.length())
+	    {
+	        if(mp.find(txt[j]) != mp.end())
+	        {
+	            mp[txt[j]] -= 1;
+	            
+	            if(mp[txt[j]] == 0)
+	            {
+	                count -= 1;
+	            }
+	        }
+	        
+	        if(j - i + 1 < k)
+	        {
+	           j++;
+	        }
+	        
+	       else if(j-i+1 == k)
+	       {
+	           if(count == 0)
+	           {
+	                ans +=1;
+	           }
+	           
+	           if(mp.find(txt[i]) != mp.end())
+	           {
+	               mp[txt[i]] += 1;
+	               
+	               if(mp[txt[i]] == 1)
+	               {
+	                    count += 1;
+	               }
+	           }
+	           
+	           i++;
+	           j++;
+	        }
+	    }
+	    
+	    return ans;
 	}
 
 };
