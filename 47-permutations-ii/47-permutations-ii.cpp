4b33c2598a4a0a76ad512c7b82a0b968ed6a1450
SHIKHAR BANSAL <shikharbansal007@gmail.com>
2022-01-22 16:29:00 +0530

    Time: 48 ms (22.69%), Space: 9.1 MB (65.92%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
-    vector<vector<int>> permuteUnique(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        backtrack(nums, 0);
-        return ans;
-    }
-    void backtrack(vector<int> nums, int index)
+    set<vector<int>> ans;
+    
+    void helper(vector<int> &nums, int curr)
     {
-        if(index == nums.size())
+        if(curr >= nums.size())
         {
-            ans.push_back(nums);
+            ans.insert(nums);
             return;
         }
         
-        for(int i = index; i < nums.size(); i++)
+        for(int i = curr; i< nums.size(); i++)
         {
-            if(nums[i] != nums[index])
-            {
-                swap(nums[i], nums[index]);
-                backtrack(nums, index+1);
-            }
-            else if(i == index)
-            {
-                backtrack(nums, index+1);
-            }
+            swap(nums[curr], nums[i]);
+            helper(nums, curr +1);
+            swap(nums[curr], nums[i]);
         }
     }
-
+   
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        
+        helper(nums, 0);
+        vector<vector<int>> vec(ans.begin(), ans.end());
+        return vec;
+    }
 };
