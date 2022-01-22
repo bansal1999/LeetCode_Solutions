ec9bbc6378333a226eaa5f6a45908b233b2ff811
SHIKHAR BANSAL <shikharbansal007@gmail.com>
2022-01-22 16:21:09 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (54.88%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,24 +1,51 @@
 class Solution {
 public:
-     void generateSubsets(vector<int>& nums,int curr,vector<int>&currSubset,set<vector<int>>&power_set)
+    
+    vector<vector<int>> result;
+    void helper(vector<int>& nums, int curr, vector<int> &subset)
     {
-        if(curr>=nums.size()){
-            power_set.insert(currSubset); // its passed by value in cpp so we don't need to make a copy
-           return;
+        result.push_back(subset);
+        
+        for(int i = curr; i< nums.size(); i++)
+        {
+            if(i > curr and nums[i-1] == nums[i])
+                continue;
+            
+            subset.push_back(nums[i]);
+            helper(nums, i +1, subset);
+            
+            subset.pop_back();
         }
-        int currval = nums[curr]; 
-        currSubset.push_back(currval);
-        generateSubsets(nums,curr+1,currSubset,power_set); // remove the current val to make the fxn for not        //consideration
-        currSubset.pop_back();
-    generateSubsets(nums,curr+1,currSubset,power_set);
     }
     
+    
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        set<vector<int>>ans;
-        vector<int>vec;
-        sort(nums.begin(),nums.end());
-        generateSubsets(nums,0,vec,ans);
-        vector<vector<int>> numbers{ans.begin(), ans.end()};
-         return numbers;
+        sort(nums.begin(), nums.end());
+        vector<int> subset;
+        helper(nums, 0, subset);
+        return result;
     }
 };
+
+
+// class Solution {
+// public:
+//     vector<vector<int>> result;
+//     void solve(vector<int>& nums , int start , vector<int>& subset){
+//         result.push_back(subset);
+//         for(int i=start ;i<nums.size();i++){
+//             if(i>start and nums[i-1] == nums[i])
+//                 continue;
+//             subset.push_back(nums[i]);
+//             solve(nums , i+1 , subset);
+//             subset.pop_back();
+//         }
+//     }
+    
+//     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+//         sort(nums.begin() , nums.end());
+//         vector<int> subset;
+//         solve(nums, 0 , subset);
+//         return result;
+//     }
+// };
