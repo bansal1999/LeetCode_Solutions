14bca705a780fc5a01c56f7c2d686587e18ecc5e
SHIKHAR BANSAL <shikharbansal007@gmail.com>
2022-01-24 23:18:05 +0530

    Time: 47 ms (45.08%), Space: 30.9 MB (94.20%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,47 +1,17 @@
 class Solution {
 public:
     bool canReach(vector<int>& arr, int start) {
-        if(arr[start] == 0)
-            return true;
-        
-        queue<int> q;
-        q.push(start);
-        
-        int n = arr.size();
-        int vis[n];
-        memset(vis, false, sizeof vis);
-        vis[start] = true;
-        
-        while(!q.empty())
+        if(start >= 0 && start < arr.size() && arr[start] >= 0)
         {
-            int idx = q.front();
-            q.pop();
-            
-            if(arr[idx] == 0)
+            if(arr[start] == 0)
                 return true;
             
-            //left part 
-            int left = idx - arr[idx];
-            if(left >= 0 && vis[left] == false)
-            {
-                if(arr[left] == 0)
-                    return true;
-    
-                vis[left] = true;
-                q.push(left);
-            }
-            
-            //right part
-            int right = arr[idx] + idx;
-            if(right < n && vis[right] == false)
-            {
-                if(arr[right] == 0)
-                    return true;
-                
-                vis[right] = true;
-                q.push(right);
-            }
+            arr[start] = -arr[start];
+            return canReach(arr, start + arr[start]) || canReach(arr, start - arr[start]);
+        
         }
+        
+       
         return false;
     }
 };
