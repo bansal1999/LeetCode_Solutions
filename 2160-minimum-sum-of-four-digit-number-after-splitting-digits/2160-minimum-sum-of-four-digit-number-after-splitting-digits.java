4cd000062a1a2dab0447edf366c07c4cc45b76a3
SHIKHAR BANSAL <shikharbansal007@gmail.com>
2022-02-05 22:46:46 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (25.00%) - LeetHub

--- 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.java
+++ 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.java
@@ -10,8 +10,8 @@ class Solution
             num /= 10;
         }
         Arrays.sort(dig); // Ascending order
-        int num1 = dig[0]*10+dig[3]; // 1st and 3rd digit
-        int num2 = dig[1]*10 + dig[2]; // 2nd and 4th digit
+        int num1 = dig[0]*10+dig[2]; // 1st and 3rd digit
+        int num2 = dig[1]*10 + dig[3]; // 2nd and 4th digit
         return num1+num2;
     }
 }
