6d5e69fbc68aa61a9b158a772cebd799a7d63508
SHIKHAR BANSAL <shikharbansal007@gmail.com>
2022-02-14 12:13:31 +0530

    Time: 4 ms (83.33%), Space: 14.1 MB (16.67%) - LeetHub

---
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        vector<int> inorder = preorder;
+        sort(inorder.begin(), inorder.end());
+        
+        
+        unordered_map<int, int> mp;
+        populateMap(mp, inorder);
+        
+        int preIndex = 0;
+        return constructTree(preorder, inorder, mp, 0, inorder.size() -1, preIndex);
+    }
+    
+    void populateMap(unordered_map<int, int> &mp, vector<int>& inorder)
+    {
+        for(int i=0; i< inorder.size(); i++)
+        {
+            mp[inorder[i]] = i;
+        }
+        return;
+    }
+    
+    TreeNode* constructTree(vector<int> &preorder, vector<int> &inorder, unordered_map<int, int> &mp, int start, int end, int &preIndex)
+    {
+        if(start > end)
+            return NULL;
+        
+        TreeNode* root = new TreeNode(preorder[preIndex]);
+        int currIndex = mp[preorder[preIndex]];
+        
+        preIndex += 1;
+        
+        root -> left = constructTree(preorder, inorder, mp, start, currIndex - 1, preIndex);
+        root -> right = constructTree(preorder, inorder, mp, currIndex + 1, end, preIndex);
+        
+        return root;
+    }
+};
