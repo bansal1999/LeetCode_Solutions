b637ab4072250bfb37e212b5d32fe3ed74c92745
SHIKHAR BANSAL <shikharbansal007@gmail.com>
2022-02-14 11:44:19 +0530

    Time: 22 ms (67.49%), Space: 26.5 MB (40.33%) - LeetHub

---
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        unordered_map<int, int> mp;
+        populateMap(mp, inorder);
+        
+        int postIndex[1] = {(int)(postorder.size() - 1)};
+        
+        return constructTree(inorder, postorder, mp, 0, inorder.size() - 1, postIndex);
+        
+    }
+    
+    void populateMap(unordered_map<int, int> &mp, vector<int> &inorder)
+    {
+        for(int i=0; i< inorder.size(); i++)
+        {
+            mp[inorder[i]] = i; 
+        }
+    }
+    
+    TreeNode* constructTree(vector<int> &inorder, vector<int> &postorder, unordered_map<int, int> &mp, int start, int end, int postIndex[1])
+    {
+        if(start > end)
+            return NULL;
+        
+        TreeNode* root = new TreeNode(postorder[postIndex[0]]);
+        int currIndex = mp[postorder[postIndex[0]]];
+        
+        postIndex[0] -= 1;
+        
+        root -> right = constructTree(inorder, postorder, mp, currIndex + 1, end, postIndex);
+        root -> left = constructTree(inorder, postorder, mp, start, currIndex - 1, postIndex);
+        
+        return root;
+        
+    }
+    
+};
