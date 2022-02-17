014689de3422036d3405d412255457c41978b9d3
SHIKHAR BANSAL <shikharbansal007@gmail.com>
2022-02-17 11:12:59 +0530

    Time: 47 ms (21.08%), Space: 15.4 MB (32.01%) - LeetHub

---
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+         
+        int n = graph.size();
+        vector<vector<int>> ans;
+        vector<bool> visited(n);
+        vector<int> currPath;
+       
+        helper(graph, ans, visited, currPath, 0, n);
+        return ans;
+    }
+    
+    void helper(vector<vector<int>> &graph, vector<vector<int>> &ans, vector<bool> &visited, vector<int> currPath, int currNode, int n)
+    {
+        if(currNode == n-1)
+        {
+            currPath.push_back(currNode);
+            ans.push_back(currPath);
+            currPath.pop_back();
+            return;
+        }
+        
+        if(visited[currNode] == true)
+            return;
+        
+        visited[currNode] = true;
+        currPath.push_back(currNode);
+        
+        for(auto it: graph[currNode])
+        {
+            helper(graph, ans, visited, currPath, it, n);
+        }
+        
+        visited[currNode] = false;
+        currPath.pop_back();
+        return;
+        
+    }
+};
