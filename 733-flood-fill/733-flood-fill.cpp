7f63e27125d419996897db67aea0637822b8225e
SHIKHAR BANSAL <shikharbansal007@gmail.com>
2022-02-27 21:50:02 +0530

    Time: 11 ms (70.40%), Space: 13.8 MB (99.23%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,38 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-        
-        if(image[sr][sc] == newColor)
-            return image;
-        
         int row = image.size();
         int col = image[0].size();
         
-        int color = image[sr][sc];
-        queue<pair<int, int>> q;
-        
-        q.push({sr, sc});
-        
-        while(!q.empty())
-        {
-            auto it = q.front();
-            q.pop();
-            
-            int currRow = it.first;
-            int currCol = it.second;
-            
-            if(currRow < 0 || currCol < 0 || currRow >= row || currCol >= col || image[currRow][currCol] != color)
-                continue;
-            
-            image[currRow][currCol] = newColor;
-            
-            q.push({currRow + 1, currCol});
-            q.push({currRow - 1, currCol});
-            q.push({currRow, currCol + 1});
-            q.push({currRow, currCol - 1});
-            
-        }
-        
+        if(image[sr][sc] == newColor)
+            return image;
+        helper(image, sr, sc, newColor, row, col, image[sr][sc]);
         return image;
+        
     }
+    
+    void helper(vector<vector<int>>& image, int cr, int cc, int newColor, int row, int col, int color)
+    {
+        if(cr < 0 || cc < 0 || cr >= row || cc >= col || image[cr][cc] != color )
+            return;
+        
+        image[cr][cc] = newColor;
+        
+        helper(image, cr + 1, cc, newColor, row, col, color);
+         helper(image, cr - 1, cc, newColor, row, col, color);
+         helper(image, cr , cc + 1, newColor, row, col, color);
+         helper(image, cr , cc - 1, newColor, row, col, color);
+        
+        return;
+        
+    }
+    
 };
