efd644cb916273c41da87b0afb685005c94b36ca
SHIKHAR BANSAL <shikharbansal007@gmail.com>
2022-03-04 17:22:29 +0530

    Time: 11 ms (73.06%), Space: 11.2 MB (13.64%) - LeetHub

---
+++ 841-keys-and-rooms/841-keys-and-rooms.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        vector<int> adj[rooms.size()];
+        
+        for(int i =0; i< rooms.size(); i++)
+        {
+            for(int j =0; j< rooms[i].size(); j++)
+            {
+                adj[i].push_back(rooms[i][j]);
+            }
+        }
+        
+        queue<int> q;
+        
+        vector<int> vis(rooms.size(),  0);
+        vis[0] =1;
+        
+        q.push(0);
+        
+        while(!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            
+            for(auto it: adj[node])
+            {
+                if(!vis[it])
+                {
+                    vis[it] = 1;
+                    q.push(it);
+                }
+            }
+        }
+        
+        for(int i =0; i< rooms.size(); i++)
+        {
+            if(!vis[i])
+                return false;
+        }
+        return true;
+        
+    }
+};
