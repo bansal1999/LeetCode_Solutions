acf8d4b73882f7fc079a8effd8f660b165c016d0
SHIKHAR BANSAL <shikharbansal007@gmail.com>
2022-03-05 21:31:27 +0530

    Time: 10 ms (81.24%), Space: 7.9 MB (94.68%) - LeetHub

---
+++ 657-robot-return-to-origin/657-robot-return-to-origin.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        
+        // unordered_map<string, int> mp;
+        
+        int upctr =0, rtctr = 0;
+        
+        for(int i =0; i< moves.size(); i++)
+        {
+           if(moves[i] == 'U')
+               upctr++;
+            else if(moves[i] == 'D')
+                upctr--;
+            
+             if(moves[i] == 'R')
+                 rtctr++;
+            else if(moves[i] == 'L')
+                rtctr--;
+        }
+        
+        // if(mp["L"] == mp["R"] && mp["U"] == mp["D"])
+        //     return true;
+        // else
+        //     return false;
+        
+        if(upctr == 0 && rtctr == 0)
+            return true;
+        else
+            return false;
+       
+    }
+};
