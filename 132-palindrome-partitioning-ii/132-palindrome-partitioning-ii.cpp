c3f5a5433730afca5a295de1903dde10c3f052e1
SHIKHAR BANSAL <shikharbansal007@gmail.com>
2022-05-27 22:40:29 +0530

    Time: 465 ms (30.35%), Space: 6.4 MB (93.46%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -12,31 +12,46 @@ private:
         return true;
     }
     
-    int helper(int i, int n, string str, vector<int> &dp)
-    {
-        if(i == n)
-            return 0;
+//     int helper(int i, int n, string str, vector<int> &dp)
+//     {
+//         if(i == n)
+//             return 0;
         
-        if(dp[i] != -1)
-            return dp[i];
+//         if(dp[i] != -1)
+//             return dp[i];
         
-        int minCost = INT_MAX;
+//         int minCost = INT_MAX;
         
-        for(int j = i; j<n; j++)
-        {
-            if(isPalindrome(i, j, str))
-            {
-                int cost = 1 + helper(j+1, n, str, dp);
-                minCost = min(minCost, cost);
-            }
-        }
-        return dp[i] = minCost;
-    }
+//         for(int j = i; j<n; j++)
+//         {
+//             if(isPalindrome(i, j, str))
+//             {
+//                 int cost = 1 + helper(j+1, n, str, dp);
+//                 minCost = min(minCost, cost);
+//             }
+//         }
+//         return dp[i] = minCost;
+//     }
     
 public:
-    int minCut(string s) {
-        int n = s.size();
-        vector<int> dp(n+1, -1);
-        return helper(0, n, s, dp) -1;
+    int minCut(string str) {
+        int n = str.size();
+        vector<int> dp(n+1, 0);
+        dp[n] = 0;
+        
+        for(int i=n-1; i>=0; i--)
+        {
+            int minCost = INT_MAX;
+            for(int j = i; j<n; j++)
+            {
+                if(isPalindrome(i, j, str))
+                {
+                    int cost = 1 + dp[j+1];
+                    minCost = min(minCost, cost);
+                }
+            }
+            dp[i] = minCost;
+        }
+        return dp[0] -1;
     }
 };
