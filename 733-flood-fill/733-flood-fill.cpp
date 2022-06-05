9ae032bc011f5432075a09f004804eccfb8a4211
SHIKHAR BANSAL <shikharbansal007@gmail.com>
2022-06-05 12:11:59 +0530

    Time: 10 ms (74.73%), Space: 14 MB (51.00%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+        
+        if(image[sr][sc] == newColor){
+            return image;
+        }
+        
         int row = image.size();
         int col = image[0].size();
         
-        if(image[sr][sc] == newColor)
-            return image;
-        helper(image, sr, sc, newColor, row, col, image[sr][sc]);
+        helper(image, sr, sc, newColor, image[sr][sc], row, col);
         return image;
-        
     }
     
-    void helper(vector<vector<int>>& image, int cr, int cc, int newColor, int row, int col, int color)
-    {
-        if(cr < 0 || cc < 0 || cr >= row || cc >= col || image[cr][cc] != color )
+    void helper(vector<vector<int>>& image, int currRow, int currCol, int newColor, int color, int row, int col) {
+        
+        if(currRow < 0 || currCol < 0 || currRow >= row || currCol >= col || image[currRow][currCol] != color){
             return;
+        }
         
-        image[cr][cc] = newColor;
+        image[currRow][currCol] = newColor;
         
-        helper(image, cr + 1, cc, newColor, row, col, color);
-         helper(image, cr - 1, cc, newColor, row, col, color);
-         helper(image, cr , cc + 1, newColor, row, col, color);
-         helper(image, cr , cc - 1, newColor, row, col, color);
+        helper(image, currRow + 1, currCol, newColor, color, row, col);
+        helper(image, currRow -1, currCol, newColor, color, row, col);
+        helper(image, currRow, currCol + 1, newColor, color, row, col);
+        helper(image, currRow, currCol - 1, newColor, color, row, col);
         
         return;
-        
     }
-    
 };
