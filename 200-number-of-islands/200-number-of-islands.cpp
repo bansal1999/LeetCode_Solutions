a38b6e1f4fc2a3f8574d210a4b06e5af70886347
SHIKHAR BANSAL <shikharbansal007@gmail.com>
2022-06-05 13:15:11 +0530

    Time: 71 ms (19.69%), Space: 12.2 MB (84.90%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,39 +1,36 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int r = grid.size();
-        int c = grid[0].size();
+        int row = grid.size();
+        int col = grid[0].size();
+        int ans = 0;
         
-        int ans=0;
-        
-        for(int i =0; i< r; i++)
-        {
-            for(int j =0; j < c;j++)
-            {
-                if(grid[i][j] == '1')
-                {
+        for(int i =0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                if(grid[i][j] == '1'){
                     ans += 1;
-                    helper(grid, i , j, r, c);
+                    helper(grid, row, col, i, j);
                 }
-                
             }
         }
-        return ans;
+       return ans;
     }
     
-    void helper(vector<vector<char>>& grid, int cr, int cc, int r, int c)
-    {
-        if(cr >= r || cc >= c || cr < 0 || cc < 0 || grid[cr][cc] == '0')
+    void helper(vector<vector<char>>& grid, int row, int col, int currRow, int currCol){
+        
+        if(currRow < 0 || currCol < 0 || currRow >= row || currCol >= col || grid[currRow][currCol] == '0')
             return;
         
-        grid[cr][cc] = '0';
+        grid[currRow][currCol] = '0';
         
-        helper(grid, cr + 1, cc, r, c);
-        helper(grid, cr - 1, cc, r, c);
-        helper(grid, cr , cc + 1, r, c);
-        helper(grid, cr , cc - 1, r, c);
+        helper(grid, row, col, currRow + 1, currCol);
+         helper(grid, row, col, currRow - 1, currCol);
+         helper(grid, row, col, currRow, currCol + 1);
+         helper(grid, row, col, currRow, currCol - 1);
         
         return;
+        
     }
+    
     
 };
