7e3abfda26316075253c57b1fe449119cfa32d1c
SHIKHAR BANSAL <shikharbansal007@gmail.com>
2022-06-05 12:24:46 +0530

    Time: 11 ms (72.14%), Space: 14.2 MB (37.55%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -9,23 +9,46 @@ public:
         int row = image.size();
         int col = image[0].size();
         
-        helper(image, sr, sc, newColor, image[sr][sc], row, col);
+        // helper(image, sr, sc, newColor, image[sr][sc], row, col);
+        
+        int color = image[sr][sc];
+        queue<pair<int, int>> q;
+        q.push({sr, sc});
+        
+        while(!q.empty()){
+            
+            int currRow = q.front().first;
+            int currCol = q.front().second;
+            q.pop();
+            
+            if(currRow < 0 || currCol < 0 || currRow >= row || currCol >= col || image[currRow][currCol] != color)
+                continue;
+            
+            image[currRow][currCol] = newColor;
+            
+            q.push({currRow + 1 , currCol});
+            q.push({currRow - 1 , currCol});
+            q.push({currRow , currCol + 1});
+            q.push({currRow , currCol - 1});
+            
+        }
+        
         return image;
     }
     
-    void helper(vector<vector<int>>& image, int currRow, int currCol, int newColor, int color, int row, int col) {
+//     void helper(vector<vector<int>>& image, int currRow, int currCol, int newColor, int color, int row, int col) {
         
-        if(currRow < 0 || currCol < 0 || currRow >= row || currCol >= col || image[currRow][currCol] != color){
-            return;
-        }
+//         if(currRow < 0 || currCol < 0 || currRow >= row || currCol >= col || image[currRow][currCol] != color){
+//             return;
+//         }
         
-        image[currRow][currCol] = newColor;
+//         image[currRow][currCol] = newColor;
         
-        helper(image, currRow + 1, currCol, newColor, color, row, col);
-        helper(image, currRow -1, currCol, newColor, color, row, col);
-        helper(image, currRow, currCol + 1, newColor, color, row, col);
-        helper(image, currRow, currCol - 1, newColor, color, row, col);
+//         helper(image, currRow + 1, currCol, newColor, color, row, col);
+//         helper(image, currRow -1, currCol, newColor, color, row, col);
+//         helper(image, currRow, currCol + 1, newColor, color, row, col);
+//         helper(image, currRow, currCol - 1, newColor, color, row, col);
         
-        return;
-    }
+//         return;
+//     }
 };
