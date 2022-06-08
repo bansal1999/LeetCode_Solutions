ad27850f46b429d8032f6ca4297d97b068b0d536
SHIKHAR BANSAL <shikharbansal007@gmail.com>
2022-06-08 23:26:29 +0530

    Time: 1560 ms (6.74%), Space: 270.5 MB (5.81%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -13,24 +13,39 @@ public:
     }
     
     void twoSumII(vector<int>& nums, int i, vector<vector<int>> &ans){
-        int lo = i + 1, hi = nums.size() - 1;
+        unordered_set<int> st;
         
-        while(lo < hi){
-            int sum = nums[i] + nums[lo] + nums[hi];
-            
-            if(sum < 0){
-                lo++;
-            }
-            else if(sum > 0){
-                hi--;
-            }
-            else{
-                ans.push_back({nums[i], nums[lo++], nums[hi--]});
-                while(lo < hi && nums[lo] == nums[lo -1]){
-                    ++lo;
+        for(int j = i +1; j < nums.size(); j++){
+            int comp = -nums[i] - nums[j];
+            if(st.find(comp) != st.end()){
+                ans.push_back({nums[i] , comp, nums[j]});
+                while(j + 1 < nums.size() && nums[j] == nums[j +1]){
+                    j++;
                 }
             }
+            st.insert(nums[j]);
         }
     }
+    
+//     void twoSumII(vector<int>& nums, int i, vector<vector<int>> &ans){
+//         int lo = i + 1, hi = nums.size() - 1;
+        
+//         while(lo < hi){
+//             int sum = nums[i] + nums[lo] + nums[hi];
+            
+//             if(sum < 0){
+//                 lo++;
+//             }
+//             else if(sum > 0){
+//                 hi--;
+//             }
+//             else{
+//                 ans.push_back({nums[i], nums[lo++], nums[hi--]});
+//                 while(lo < hi && nums[lo] == nums[lo -1]){
+//                     ++lo;
+//                 }
+//             }
+//         }
+//     }
     
 };
