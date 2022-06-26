09fa619fb2b22723ee72897644e0f7a21fadb7a9
SHIKHAR BANSAL <shikharbansal007@gmail.com>
2022-06-26 22:32:32 +0530

    Time: 71 ms (10.61%), Space: 30.9 MB (20.68%) - LeetHub

--- 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
@@ -9,18 +9,55 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// Self Notes:
+// \U0001f347 Formula is (2^TreeLevel - 1). Only works for perfect tree.
+// \U0001f347 To determine if its a perfect tree, go all the way down and count the nodes on left and right side, If they match, its a perfect tree and our formula applies.
+// \U0001f347 If its not a perfect tree, we go on left and right subtree and again determine if they are perfect tree.
+// \U0001f347 When we have our left and right heights, we do (1 + left + right)
+// \U0001f347 If we reach null, return 0
+// \U0001f347 C++ note: 1 << n is the same as raising 2 to the power n, or 2^n
+// Time complexiity: O(logn)^2;
+
 class Solution {
 public:
+    
     int countNodes(TreeNode* root) {
-        
-        int cnt =0;
-        if(root == NULL)
+        if(root == NULL){
             return 0;
+        }
         
-        cnt++;
-        cnt += countNodes(root-> left);
-        cnt += countNodes(root -> right);
+        int leftHeight = findLeftHeight(root);
+        int rightHeight = findRightHeight(root);
         
-        return cnt;
+        if(leftHeight == rightHeight){
+            return (1 << leftHeight) -1;        // (2^n - 1) 
+        }
+        
+        return 1 + countNodes(root -> left) + countNodes(root -> right);
+        
     }
+    
+    int findLeftHeight(TreeNode* node){
+        int height = 0;
+        while(node){
+            height++;
+            node = node -> left;
+        }
+        
+        return height;
+    }
+    
+    int findRightHeight(TreeNode* node){
+        int height = 0;
+        while(node){
+            height++;
+            node = node -> right;
+        }
+        
+        return height;
+    }
+    
+    
+    
 };
