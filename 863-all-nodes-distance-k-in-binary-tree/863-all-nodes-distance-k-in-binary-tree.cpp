b23ebaee833fb0ccd8b8e473e57a64837938d4ae
SHIKHAR BANSAL <shikharbansal007@gmail.com>
2022-06-26 23:20:40 +0530

    Time: 15 ms (24.60%), Space: 13.2 MB (36.11%) - LeetHub

--- 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
@@ -7,85 +7,84 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+// Self Notes:
+// \U0001f34b Mark each node to its parent to traverse upwards
+// \U0001f34b We will do a BFS traversal starting from the target node
+// \U0001f34b As long as we have not seen our node previously, Traverse up, left, right until reached Kth distance
+// \U0001f34b when reached Kth distance, break out of BFS loop and remaining node's values in our queue is our result
+
 class Solution {
 public:
-    void markParent(TreeNode* root, unordered_map<TreeNode*, TreeNode*> &parentStore)
-    {
+    void markParents(unordered_map<TreeNode*, TreeNode*> &parentMap, TreeNode* root){
         queue<TreeNode*> q;
         q.push(root);
         
-        while(!q.empty())
-        {
-            TreeNode* currNode = q.front();
+        while(!q.empty()){
+            TreeNode* curr = q.front();
             q.pop();
-            if(currNode -> left)
-            {
-                parentStore[currNode -> left] = currNode;
-                q.push(currNode -> left);
-                
+            
+            if(curr -> left){
+                parentMap[curr -> left] = curr;
+                q.push(curr -> left);
             }
             
-            if(currNode -> right)
-            {
-                parentStore[currNode -> right] = currNode;
-                q.push(currNode -> right) ;
+            if(curr -> right){
+                parentMap[curr -> right] = curr;
+                q.push(curr -> right);
             }
-            
         }
     }
     
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        unordered_map<TreeNode*, TreeNode*> parentStore;
-        markParent(root,parentStore);
+        unordered_map<TreeNode*, TreeNode*> parentMap;  // node -> parent
+        markParents(parentMap, root);
         
         unordered_map<TreeNode*, bool> visited;
-        queue<TreeNode*> qu;
-        qu.push(target);
-        visited[target] = true;
-        int currLvl = 0;
+        queue<TreeNode*> q;
         
-        while(!qu.empty())
-        {
-            int sz = qu.size();
-            if(currLvl == k)
+        visited[target] = true;
+        q.push(target);
+        
+        int currLevel = 0;
+        
+        while(!q.empty()){
+            
+            int size = q.size();
+            if(currLevel == k){
                 break;
+            }
+            currLevel++;
             
-            currLvl += 1;
-            
-            for(int i=0; i< sz; i++)
-            {
-                TreeNode* curr = qu.front();
-                qu.pop();
+            for(int i = 0; i < size; i++){
+                TreeNode* curr = q.front();
+                q.pop();
                 
-                if(curr -> left && !visited[curr -> left])
-                {
-                    qu.push(curr -> left);
+                if(curr -> left && !visited[curr -> left]){
+                    q.push(curr -> left);
                     visited[curr -> left] = true;
                 }
                 
-                if(curr -> right && !visited[curr -> right])
-                {
-                    qu.push(curr -> right);
-                    visited[curr -> right]  = true;
+                if(curr -> right && !visited[curr -> right]){
+                    q.push(curr -> right);
+                    visited[curr -> right] = true;
                 }
                 
-                if(parentStore[curr] && !visited[parentStore[curr]])
-                {
-                    qu.push(parentStore[curr]);
-                    visited[parentStore[curr]] = true;
+                if(parentMap[curr] && !visited[parentMap[curr]]){
+                    q.push(parentMap[curr]);
+                    visited[parentMap[curr]] = true;
                 }
+                
             }
-        }
-        
-        vector<int> res;
-        while(!qu.empty())
-        {
-            TreeNode* current = qu.front();
-            qu.pop();
             
-            res.push_back(current -> val);
         }
-        return res;
         
+        vector<int> result;
+        while(!q.empty()){
+            TreeNode* curr = q.front();
+            q.pop();
+            result.push_back(curr -> val);
+        }
+        return result;
     }
 };
