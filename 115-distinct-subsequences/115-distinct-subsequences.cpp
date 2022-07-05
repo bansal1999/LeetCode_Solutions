46888004bf52c34af6ba3c52d324c725c8ef099a
SHIKHAR BANSAL <shikharbansal007@gmail.com>
2022-07-05 22:29:25 +0530

    Time: 67 ms (36.59%), Space: 18.5 MB (58.05%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -1,30 +1,38 @@
 class Solution {
 public:
-    int helper(int i, int j, string &s, string &t, vector<vector<int>> &dp)
-    {
+    int helper(string &s, string &t, int ind1, int ind2, vector<vector<int>> &dp){
         
-        if(j < 0 )
+        if(ind2 < 0){
             return 1;
-        if(i < 0)
+        }
+        
+        if(ind1 < 0){
             return 0;
+        }
         
-        if(dp[i][j] != -1)
-            return dp[i][j];
         
-        if(s[i] == t[j])
-            return dp[i][j] = helper(i-1, j-1, s, t, dp) + helper(i-1, j, s, t, dp);
+        if(dp[ind1][ind2] != -1){
+            return dp[ind1][ind2];
+        }
         
-        return dp[i][j] = helper(i-1, j, s, t, dp);
+        if(s[ind1] == t[ind2]){
+            int leave = helper(s, t, ind1-1, ind2, dp);
+            int take = helper(s, t, ind1 - 1, ind2 - 1, dp);
+            return dp[ind1][ind2] = (leave + take);
+        }
+        else{
+            return dp[ind1][ind2] = helper(s, t, ind1 -1, ind2, dp);
+        }
         
     }
-    
     
     
     int numDistinct(string s, string t) {
         int n = s.size();
         int m = t.size();
         
-        vector<vector<int>>dp (n, vector<int>(m, -1));
-        return helper(n-1, m-1, s, t, dp);
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        
+        return helper(s, t, n -1, m - 1, dp);
     }
 };
