0f6c9abb0cd01c5682ce61c29859668cbc38458a
SHIKHAR BANSAL <shikharbansal007@gmail.com>
2022-07-25 20:52:01 +0530

    Time: 16 ms (30.15%), Space: 13 MB (79.07%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -1,56 +1,52 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int  row = grid.size(), col= grid[0].size();
-        int freshOranges = 0;
+        int row = grid.size();
+        int col = grid[0].size();
         
+        int freshOranges = 0;
         queue<pair<int, int>> q;
         
-        for(int i =0; i< row; i++)
-        {
-            for(int j =0; j < col; j ++)
-            {
-                if(grid[i][j] == 1)
-                    freshOranges++;
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
                 
-                if(grid[i][j] == 2)
+                if(grid[i][j] == 1){
+                    freshOranges++;
+                }
+                
+                if(grid[i][j] == 2){
                     q.push({i, j});
+                }
             }
         }
         
-        int time =0;
-        vector<pair<int, int>> dirs = {{-1,0} ,{0, 1}, {1, 0}, {0, -1}};
+        int time = 0;
+        vector<pair<int, int>> dirs = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
         
-        while(!q.empty() && freshOranges!= 0)
-        {
-            int qsize = q.size();
-            for(int i =0; i< qsize; i++)
-            {
+        while(!q.empty() && freshOranges != 0){
+            int size = q.size();
+            
+            for(auto i = 0; i < size; i++){
                 auto curr = q.front();
                 q.pop();
-
-                int x = curr.first;
-                int y = curr.second;
-
-                for(auto it: dirs)
-                {
-                    int newX = it.first + x;
-                    int newY = it.second + y;
-
-                    if(newX >= 0 && newX < row && newY >= 0 && newY < col && grid[newX][newY] == 1)
-                    {
+                
+                int currX = curr.first;
+                int currY = curr.second;
+                
+                for(auto &it: dirs){
+                    int newX = it.first + currX;
+                    int newY = it.second + currY;
+                    
+                    if(newX >= 0 && newX < row && newY >= 0 && newY < col && grid[newX][newY] == 1){
                         grid[newX][newY] = 2;
                         freshOranges--;
                         q.push({newX, newY});
-
                     }
                 }
+                
             }
-            
             time++;
         }
-        
         return freshOranges == 0 ? time : -1;
-        
     }
 };
