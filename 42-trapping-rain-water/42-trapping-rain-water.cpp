51b0ec8aa2a2bc6984b8a57653792940f9eee0cd
SHIKHAR BANSAL <shikharbansal007@gmail.com>
2022-07-28 01:25:43 +0530

    Time: 37 ms (13.74%), Space: 20.9 MB (5.87%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -5,20 +5,23 @@ public:
         
         vector<int> mxl(n);
         vector<int> mxr(n);
-        
-        mxl[0] = arr[0];
-        
+         
+        // calculate the maximum element in the LEFT
+         mxl[0] = arr[0];
         for(int i = 1; i<n; i++)
         {
             mxl[i] = max(mxl[i-1],  arr[i]);
         }
         
+        // calculate the maximum element in the LEFT
         mxr[n-1] = arr[n-1];
         for(int i = n -2; i>= 0; i-- )
         {
             mxr[i] = max(mxr[i+1], arr[i]);
         }
         
+        // water will be store in between of mxr and mxl
+        // take  min of mxr and mxl 
         vector<int> water(n);
         for(int i =0; i< n; i++)
         {
