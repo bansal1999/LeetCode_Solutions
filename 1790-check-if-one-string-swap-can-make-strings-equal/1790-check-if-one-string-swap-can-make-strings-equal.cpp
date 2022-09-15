3e6667d54626e80b325bed0f075e25bb90ad6e8a
SHIKHAR BANSAL <shikharbansal007@gmail.com>
2022-09-15 20:45:18 +0530

    Time: 4 ms (40.41%), Space: 6.4 MB (22.55%) - LeetHub

---
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool areAlmostEqual(string s1, string s2) {
+        vector<int> diffPos;
+        
+        for(int i =0; i < s1.size(); i++){
+            if(s1[i] != s2[i]){
+                diffPos.push_back(i);
+            }
+            if(diffPos.size() > 2){
+                return false;
+            }
+        }
+        
+        if(diffPos.empty()){
+            return true;
+        }
+        
+        if(diffPos.size() == 2){
+            swap(s1[diffPos[0]] , s1[diffPos[1]]);
+        }
+        
+        return s1 == s2;
+    }
+};
