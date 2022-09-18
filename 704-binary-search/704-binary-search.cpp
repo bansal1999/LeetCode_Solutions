57367132ecc282aaa2425a0c9793e98abdf62109
SHIKHAR BANSAL <shikharbansal007@gmail.com>
2022-09-18 09:21:48 +0530

    Time: 46 ms (81.44%), Space: 27.6 MB (63.23%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -4,20 +4,21 @@ public:
         int low = 0;
         int high = nums.size() - 1;
         
-        while(low <= high)
-        {
+        while(low <= high){
             int mid = low + (high - low)/2;
-            if(nums[mid] == target)
+            
+            if(nums[mid] == target){
                 return mid;
-            else if(nums[mid] < target)
-            {
-                low = mid +1;
             }
-            else
-            {
-                high = mid -1;
+            else if(nums[mid] < target){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
             }
         }
+        
         return -1;
+        
     }
 };
